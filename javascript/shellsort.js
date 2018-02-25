40e2cb124b17f84f427cd66bd5b1ef576d104bb1
caesarii <iwangqinghe@gmail.com>
2018-02-25 14:12:20 +0800

    重构 js shell sort

--- javascript/shellsort.js
+++ javascript/shellsort.js
@@ -2,32 +2,37 @@ const {log, exchange} = require('./utils')
 
 const floor = Math.floor
 
+const half = n => {
+    return Math.floor(n / 2)
+}
+
 const shellsort = (list) => {
-    // 增量序列 [ len / 2, pre / 2, ...]
-    // 每一趟增量 incre 排序之后, 对于元素 i, i + incre, i + incre*2.. 是排序好的, 直到 i, i+1, i+2 是排序好的
     
     const length = list.length
-    let temp
-    let j
-    for(let increment = floor(length / 2); increment > 0; increment = floor(increment / 2)) {
-        // 增量序列 [ 5, 2, 1 ]
-        for(let i = increment; i < length; i++) {
-            temp = list[i]
-            for(j = i; j >= increment; j -= increment) {
-                if(temp < list[j - increment]) {
-                    list[j] = list[j - increment]
-                } else {
-                    break
+    
+    // 遍历增量序列
+    for(let increment = half(length); increment > 0; increment = half(increment)) {
+        log('increment', increment)
+        
+        // 遍历数组: 对元素进行当前增量的排序
+        for(let current = increment; current < length; current++) {
+            log('i', current)
+            
+            // 对当前元素进行基于增量的插入排序
+            for(let prev = current ; prev >= increment; prev -= increment) {
+                
+                if(list[prev] < list[prev - increment]) {
+                    exchange(list, prev, prev - increment)
                 }
             }
-            list[j] = temp
         }
+        
     }
-    return list
+    
 }
 
 if(require.main === module) {
     const list = [38, 545, 6, 9, 324, 1, 4, 564, 17, 754]
-    const sorted = shellsort(list)
-    console.log(sorted)
+    shellsort(list)
+    log(list)
 }
