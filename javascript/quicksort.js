70acdcc6975a591f2ddd1ba39d122fc46a5f7690
caesarii <iwangqinghe@gmail.com>
2018-02-26 15:28:43 +0800

    Revert "重构 js quick sort"
    This reverts commit 11eb166902d02bfb9419e097f13ec1d6f034fdc5.

--- javascript/quicksort.js
+++ javascript/quicksort.js
@@ -1,4 +1,5 @@
 const {log, exchange} = require('./utils')
+const insertsort = require('./insertsort')
 
 // 三数中值排序法
 const median = (list, left, right) => {
@@ -15,7 +16,10 @@ const median = (list, left, right) => {
         exchange(list, center, right)
     }
     
-    return list[center]
+    // 将枢纽元放置在
+    exchange(list, center, right - 1)
+    // log('median', list)
+    return list[right - 1]
 }
 
 const quicksortIter = (list, left, right, mode) => {
@@ -29,24 +33,23 @@ const quicksortIter = (list, left, right, mode) => {
         }
         return
     }
-    
     // 枢纽元
+    
     const pivot = median(list, left, right)
-    // 将枢纽元放置在 right - 1
-    exchange(list, Math.floor((left + right) / 2), right - 1)
     log('pivot', pivot)
     
-    // 划分集合
-    let i = left + 1 // left 一定小于 pivot
-    let j = right - 2 // right 一定大于 pivot, right - 1 放置 pivot
-    while(true)  {
+    let i = left // left 一定小于 pivot
+    let j = right - 1 // right 一定大于 pivot, right - 1 放置 pivot
+    while(true) {
         // i 指向大于 pivot 的元素
-        while(list[i] < pivot) {
-            i += 1
+        
+        while(list[++i] < pivot) {
+        
         }
         // j 指向小于 pivot 的元素
-        while(list[j] > pivot) {
-            j -= 1
+        
+        while(list[--j] > pivot) {
+        
         }
         
         // 交换元素
@@ -60,7 +63,6 @@ const quicksortIter = (list, left, right, mode) => {
     exchange(list, i, right - 1)
     log('new list', list)
     log(mode, '\n')
-    // 递归
     quicksortIter(list, left, i - 1, `${mode} left`)
     quicksortIter(list, i + 1, right, `${mode} right`)
 }
@@ -68,12 +70,12 @@ const quicksortIter = (list, left, right, mode) => {
 const quicksort = list => {
     const len = list.length
     quicksortIter(list, 0, len - 1, 'first')
-    
+    return list
 }
 
 
 if(require.main === module) {
     const list = [38, 545, 6, 9, 324, 1, 4, 564, 17, 754]
-    quicksort(list)
-    log(list)
+    const sorted = quicksort(list)
+    console.log(sorted)
 }
