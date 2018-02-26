0a8aed326b67196e7c0c9bd6f241e1101a6cfa4f
caesarii <iwangqinghe@gmail.com>
2018-02-26 11:39:08 +0800

    重构 python heap sort

--- python/heapsort.py
+++ python/heapsort.py
@@ -2,19 +2,16 @@
 from utils import log, exchange
 from math import floor
 
-def perdown(list, i, length):
-    temp = list[i]
-    while i * 2 + 1 < length:
-        child = i * 2 + 1
-        if child != length - 1 and list[child + 1] > list[child]:
+def perdown(list, current, length):
+    child = current * 2 + 1
+    while child < length:
+        if child + 1 != length and list[child + 1] > list[child]:
             child = child + 1
-        if temp < list[child]:
-            list[i] = list[child]
-        else:
-            break
 
-        i = child
-    list[i] = temp
+        if list[current] < list[child]:
+            exchange(list, current, child)
+        current = child
+        child = current * 2 + 1
 
 def heapsort(list):
     length = len(list)
@@ -25,9 +22,7 @@ def heapsort(list):
         exchange(list, 0, i)
         perdown(list, 0, i)
 
-    return list
-
 if __name__ == '__main__':
     list = [38, 545, 6, 9, 324, 1, 4, 564,17, 754]
-    sorted = heapsort(list)
-    log(sorted)
+    heapsort(list)
+    log(list)
