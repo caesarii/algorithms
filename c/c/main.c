8e48766af34d67cea1fc3e4138d7d0f155c9713b
caesarii <iwangqinghe@gmail.com>
2018-02-26 16:40:05 +0800

    test

--- c/c/main.c
+++ c/c/main.c
@@ -7,9 +7,24 @@
 //
 
 #include <stdio.h>
+#include "insertsort.h"
+#include "utils.h"
+
+
+void
+testInsertsort(void) {
+    type list[] = {38, 545, 6, 9, 324, 1, 4, 564,17, 754};
+    int length = 10;
+    insertsort(list, length);
+//    printf("%d\n", list[1]);
+    qinglog(list, length);
+    
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    testInsertsort();
+    
     printf("Hello, World!\n");
     return 0;
 }
