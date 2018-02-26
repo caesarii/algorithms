f8b2b50454a6f3dc0acb9b757c67f514a4518f58
caesarii <iwangqinghe@gmail.com>
2018-02-26 16:39:54 +0800

    c insert sort

---
+++ c/c/insertsort.c
@@ -0,0 +1,24 @@
+//
+//  insertsort.c
+//  c
+//
+//  Created by qinghe on 26/02/2018.
+//  Copyright © 2018 qinghe. All rights reserved.
+//
+
+
+#include "insertsort.h"
+#include "utils.h"
+
+void
+insertsort(type *list, int numberOfElements) {
+    int prev;
+    for(int current = 1; current < numberOfElements; current++) {
+        for(prev = current; prev > 0; prev --) {
+            if(list[prev] < list[prev - 1]) {
+                exchange(list, prev, prev - 1);
+            }
+        }
+    }
+    
+}
---
+++ c/c/insertsort.h
@@ -0,0 +1,17 @@
+//
+//  insertsort.h
+//  c
+//
+//  Created by qinghe on 26/02/2018.
+//  Copyright © 2018 qinghe. All rights reserved.
+//
+
+#ifndef insertsort_h
+#define insertsort_h
+
+#include <stdio.h>
+
+typedef int type;
+void
+insertsort(type *list, int numberOfElements);
+#endif /* insertsort_h */
