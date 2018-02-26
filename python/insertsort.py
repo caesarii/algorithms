0114a21ba38e3ee910c634cea82f5c44cac7f471
caesarii <iwangqinghe@gmail.com>
2018-02-26 11:40:19 +0800

    重构 py insert sort

--- python/insertsort.py
+++ python/insertsort.py
@@ -7,12 +7,10 @@ def insertsort(list):
         for prev in range(current, 0, -1):
             if list[prev] < list[prev - 1]:
                 exchange(list, prev, prev - 1)
-    return list
-
 
 if __name__ == '__main__':
     list = [38, 545, 6, 9, 324, 1, 4, 564,17, 754]
-    sorted = insertsort(list)
-    log(sorted)
+    insertsort(list)
+    log(list)
 
 
