0b5061ebd653b80adf70187d15ebb5242c8e2601
caesarii <iwangqinghe@gmail.com>
2018-02-26 15:46:56 +0800

    创建 xcode c 工程

---
+++ c/c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c
+//
+//  Created by qinghe on 26/02/2018.
+//  Copyright © 2018 qinghe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
