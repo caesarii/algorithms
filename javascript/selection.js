5b88ee9c7dc8749ec92496b02959e772452efb05
caesarii <iwangqinghe@gmail.com>
2018-11-27 09:25:19 +0800

    selection

---
+++ javascript/selection.js
@@ -0,0 +1,30 @@
+const {log, exchange} = require('./utils')
+
+const selection = (A) => {
+    // 遍历数组
+    for(let j = 0; j < A.length; j++){
+        // k: 已排序元素中的最大值
+        let k = j;
+        // 从当前元素往后遍历
+        let i = j + 1
+        // 如果比较元素小于当前元素，则记录
+        while(i < A.length) {
+            if (A[i] < A[k]) {
+                k = i;
+            }
+            i = i + 1
+        }
+        const min = A[k]
+        A[k] = A[j]
+        A[j] = min
+    }
+}
+
+
+if(require.main === module) {
+    const list = [38, 545, 6, 9, 324, 1, 4, 564,17, 754]
+    selection(list)
+    log(list)
+}
+
+module.exports = selection
